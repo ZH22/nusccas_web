c339004a218e3951061480c51a9354befc9bb192
zhenghao <zhenghao3210@gmail.com>
2025-05-21 15:05:53 +0800

    Integrated Supabase Auth + Added Login Feature

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -4,10 +4,15 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
+interface LoginFormProps extends React.ComponentProps<"div"> {
+  onLogin: (formData: FormData) => Promise<void>;
+}
+
 export function LoginForm({
   className,
+  onLogin,
   ...props
-}: React.ComponentProps<"div">) {
+}: LoginFormProps) {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
@@ -23,6 +28,7 @@ export function LoginForm({
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
                 <Input
+                  name="email"
                   id="email"
                   type="email"
                   placeholder="m@example.com"
@@ -39,9 +45,9 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input name="password" id="password" type="password" required />
               </div>
-              <Button type="submit" className="w-full">
+              <Button formAction={ onLogin } type="submit" className="w-full">
                 Login
               </Button>
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
