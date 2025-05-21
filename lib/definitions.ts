fc8f514360d5e453d6a4d0da2ca339c548d4fa04
zhenghao <zhenghao3210@gmail.com>
2025-05-21 14:15:35 +0800

    Added Client + Server Components for Supabase Integration (+ Middleware)

---
+++ lib/definitions.ts
@@ -0,0 +1,4 @@
+// Type definitions for proper format of data coming from database 
+
+// Emtpy for now (but to confirm later)
+// Manual way of defining data schema -> may be better to use software like Prisma or Drizzle to automatically generate
