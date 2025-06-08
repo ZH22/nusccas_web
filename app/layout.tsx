505f1811c3a18c2a23da3f88a735464f50148a81
zhenghao <zhenghao3210@gmail.com>
2025-06-08 14:35:43 +0800

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -39,6 +41,8 @@ export default function RootLayout({
             </div>
           </AuthProvider>
         </ThemeProvider>
+
+        <Analytics />
       </body>
     </html>
 
