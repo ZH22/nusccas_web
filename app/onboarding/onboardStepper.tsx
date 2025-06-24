66197633e4bcbc882bece41e0ec11a8440b361f2
Ronith <ronithsaju2002@gmail.com>
2025-06-25 00:42:21 +0800

    Integrated Recommendation System - API call to Lightning AI Studio to run model

--- app/onboarding/onboardStepper.tsx
+++ app/onboarding/onboardStepper.tsx
@@ -3,7 +3,7 @@
 import Stepper, {
   Step,
 } from "@/components/reactBitsComponents/Stepper/Stepper";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 // import { Combobox } from "@/components/combo-box";
 // import ComboboxFromFile from "@/components/combo-box-from-file";
 // import { MultiSelectCombobox } from "@/components/multi-select-combo-box";
@@ -19,22 +19,131 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { DynamicInputs } from "@/components/dynamic-inputs";
+import { type User } from "@supabase/supabase-js";
+import { createClient } from "@/utils/supabase/client";
 
-export default function OnboardStepper() {
-  const [name, setName] = useState("");
-  const [race, setRace] = useState("");
-  const [religion, setReligion] = useState("");
+export default function OnboardStepper({ user }: { user: User | null }) {
+  const [firstName, setFirstName] = useState<string>("");
+  const [race, setRace] = useState<string>("");
+  const [religion, setReligion] = useState<string>("");
   const [majors, setMajors] = useState<string[]>([]);
   const [minors, setMinors] = useState<string[]>([]);
   const [ccas, setCcas] = useState<string[]>([]);
   const [interests, setInterests] = useState<string[]>([]);
+
+  const supabase = createClient();
+
+  // Get Profile information from database =============================
+  const getProfile = useCallback(async () => {
+    try {
+      const { data, error, status } = await supabase
+        .from("profiles")
+        .select("first_name, race, religion, majors, minors, ccas, interests")
+        .eq("id", user?.id)
+        .single();
+      if (error && status !== 406) {
+        throw error;
+      }
+      if (data) {
+        data["first_name"] && setFirstName(data["first_name"]); // "&&" used so that if data is null, placeholder shows
+        data["race"] && setRace(data["race"]);
+        data["religion"] && setReligion(data["religion"]);
+        data["majors"] && setMajors(data["majors"]);
+        data["minors"] && setMinors(data["minors"]);
+        data["ccas"] && setCcas(data["ccas"]);
+        data["interests"] && setInterests(data["interests"]);
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Error retrieving user data :(");
+    }
+  }, [user, supabase]);
+
+  // Performs after page renders =======================================
+  useEffect(() => {
+    getProfile();
+  }, [user, getProfile]);
+
+  // Update Profile Function
+  async function updateProfileAndRecommendCcas({
+    firstName,
+    race,
+    religion,
+    majors,
+    minors,
+    ccas,
+    interests,
+  }: {
+    firstName: string;
+    race: string;
+    religion: string;
+    majors: string[];
+    minors: string[];
+    ccas: string[];
+    interests: string[];
+  }) {
+    try {
+      const { error } = await supabase
+        .from("profiles")
+        .update({
+          first_name: firstName,
+          updated_at: new Date().toISOString(),
+          race: race,
+          religion: religion,
+          majors: majors,
+          minors: minors,
+          ccas: ccas,
+          interests: interests,
+        })
+        .eq("id", user?.id as string);
+      if (error) throw error;
+      // alert("Profile updated!");
+    } catch (error) {
+      console.log(error);
+      alert("Error uploading the results :(");
+    }
+
+    // API call to run CCA recommender AI model on user & CCA data in Lightning AI Studio
+    const LIT_SERVER_API_KEY = process.env.NEXT_PUBLIC_LIT_SERVER_API_KEY;
+    if (!LIT_SERVER_API_KEY) {
+      throw new Error("LIT_SERVER_API_KEY is not defined in .env.local");
+    }
+    const url =
+      "https://8000-01jyc5f4xnnk2wrzcmybg4vbgr.cloudspaces.litng.ai/predict";
+    const options = {
+      method: "POST",
+      headers: {
+        "X-API-Key": LIT_SERVER_API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user_id: user?.id as string }),
+    };
+
+    try {
+      fetch(url, options); // no response expected
+      //const response = await fetch(url, options);
+      //const data = await response.json();
+      //console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <Stepper
       initialStep={1}
       // onStepChange={(step) => { console.log(step); }}
       onFinalStepCompleted={() =>
-        console.log(name, race, religion, majors, minors, ccas, interests)
+        // console.log(firstName, race, religion, majors, minors, ccas, interests)
+        updateProfileAndRecommendCcas({
+          firstName,
+          race,
+          religion,
+          majors,
+          minors,
+          ccas,
+          interests,
+        })
       }
       backButtonText="Previous"
       // backButtonProps={{ className: "cursor-pointer" }}
@@ -48,12 +157,12 @@ export default function OnboardStepper() {
       </Step>
 
       <Step>
-        <h2 className="text-2xl font-bold">What's your name?</h2>
+        <h2 className="text-2xl font-bold">What's your first name?</h2>
         <br />
         <Input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
+          value={firstName == "" ? undefined : firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder="First Name"
         />
       </Step>
 
--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,17 +1,19 @@
-import { redirect } from 'next/navigation'
-import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 
-import OnboardStepper from './onboardStepper';
+import OnboardStepper from "./onboardStepper";
 
-export default async function Onboarding() { 
-  
-   const supabase = await createClient();
+export default async function Onboarding() {
+  const supabase = await createClient();
 
   // Check if user authenticated
-  const {data: { user }, error} = await supabase.auth.getUser()
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
   // Redirect if unauthenticated
   if (error || !user) {
-    redirect('/login')
+    redirect("/login");
   }
 
   // Grab data about user's preferences (if any) and pass over to client component
@@ -19,7 +21,7 @@ export default async function Onboarding() {
   return (
     <>
       <h1>Onboarding</h1>
-      <OnboardStepper /> 
+      <OnboardStepper user={user} />
     </>
-  )
+  );
 }
