260c503181b89b4e1770cf8293dae445ad699229
zhenghao <zhenghao3210@gmail.com>
2025-06-30 13:16:33 +0800

    Changed Lightning AI API route to Deployment Instance

--- app/api/lightning_ai/run_cca_recommender/route.ts
+++ app/api/lightning_ai/run_cca_recommender/route.ts
@@ -4,8 +4,10 @@ export async function POST(request: Request) {
     try {
         const body = await request.json();
 
-        const url = "https://8000-01jyc5f4xnnk2wrzcmybg4vbgr.cloudspaces.litng.ai/predict"
-
+        // const url = "https://8000-01jyc5f4xnnk2wrzcmybg4vbgr.cloudspaces.litng.ai/predict"
+        const url = process.env.NEXT_PUBLIC_LIT_SERVER_API_URL as string;
+        const LIT_SERVER_AUTHORIZATION = process.env.NEXT_PUBLIC_LIT_SERVER_AUTHORIZATION as string;
+        
         const LIT_SERVER_API_KEY = process.env.NEXT_PUBLIC_LIT_SERVER_API_KEY;
         if (!LIT_SERVER_API_KEY) {
             return NextResponse.json({ error: "LIT_SERVER_API_KEY is not defined in .env.local" }, { status: 500 });
@@ -16,11 +18,14 @@ export async function POST(request: Request) {
             method: "POST",
             headers: {
                 "X-API-Key": LIT_SERVER_API_KEY,
+                "Authorization": LIT_SERVER_AUTHORIZATION,
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(body),
         };
 
+        console.log(url)
+        console.log(options)
         fetch(url, options); // no response expected
         return NextResponse.json({ success: true });
     } catch (error: unknown) {
